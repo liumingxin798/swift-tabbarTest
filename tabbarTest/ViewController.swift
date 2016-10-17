72c2c70831615136a5784b16c2cb859f6113e896
liumingxin <1061362426@qq.com>
2016-10-17 14:38:16 +0800

    Initial Commit

---
+++ tabbarTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tabbarTest
+//
+//  Created by 刘明鑫 on 16/10/17.
+//  Copyright © 2016年 刘明鑫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
